afdc2abebffb85baab2dc9ba2dfe603e978cabdb
gpgaliperez <gp.galiperez@gmail.com>
2021-09-08 23:29:14 -0300

    Initial commit

---
+++ src/main/java/com/modeladosemanticos/sistemadebecas/SistemaDeBecasApplication.java
@@ -0,0 +1,13 @@
+package com.modeladosemanticos.sistemadebecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeBecasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeBecasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modeladosemanticos/sistemadebecas/SistemaDeBecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modeladosemanticos.sistemadebecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeBecasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
